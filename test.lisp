a9d623a1fa30cea0ca2398c6ab7738d3f8e7d472
Eugene Zaikonnikov <eugene@funcall.org>
2016-08-08 19:19:25 +0200

    expanding on tests

--- test.lisp
+++ test.lisp
@@ -1,5 +1,13 @@
 (in-package #:jpeg)
 
-(time (multiple-value-bind (buf h w nc)
-	  (decode-image #P"/home/eugene/Pictures/nyc.jpg")
-	(encode-image #P"/tmp/test.jpg" buf nc h w)))
+(defun test-reencode (&optional (pathname #P"/home/eugene/Pictures/nyc.jpg"))
+  (time (multiple-value-bind (buf h w nc)
+	    (decode-image pathname)
+	  (encode-image #P"/tmp/test.jpg" buf nc h w))))
+
+(defun test-reencode-prealloc (&optional (pathname #P"/home/eugene/Pictures/nyc.jpg"))
+  (multiple-value-bind (h w ncomp)
+      (jpeg-file-dimensions pathname)
+    (let ((buf (allocate-buffer h w ncomp)))
+      (decode-image pathname buf)
+      (encode-image #P"/tmp/test.jpg" buf ncomp h w))))
