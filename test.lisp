99a39d3757039a123ef09ab8c23907ad26253585
Eugene Zaikonnikov <eugene@funcall.org>
2016-11-17 07:45:32 +0100

    tidy up tests

--- test.lisp
+++ test.lisp
@@ -1,12 +1,9 @@
-(cl:defpackage :jpeg-test
-  (:use #:cl #:jpeg))
-
-(cl:in-package :jpeg-test)
+(cl:in-package :jpeg)
 
 (defun test-reencode (&optional (pathname #P"/home/eugene/Pictures/nyc.jpg"))
   (time (multiple-value-bind (buf h w nc)
-	    (decode-image pathname)
-	  (encode-image #P"/tmp/test.jpg" buf nc h w))))
+	    (jpeg:decode-image pathname)
+	  (jpeg:encode-image #P"/tmp/test.jpg" buf nc h w))))
 
 (defun test-reencode-prealloc (&optional (pathname #P"/home/eugene/Pictures/nyc.jpg"))
   (multiple-value-bind (h w ncomp)
@@ -15,19 +12,11 @@
       (decode-image pathname buf)
       (encode-image #P"/tmp/test.jpg" buf ncomp h w))))
 
-(defun test-image (filename)
-  (reduce #'merge-pathnames (list filename "images/")
-          :from-end t
-          :initial-value (asdf:component-pathname
-                          (asdf:find-system "cl-jpeg"))))
-
-(ensure-directories-exist (output-image ""))
-
 (defparameter *gray-q-tabs* (vector jpeg::+q-luminance+))
 
-(defun test-encode (&optional (output (test-image "simple.jpeg")))
+(defun test-encode-grayscale (&optional (output #P"/tmp/gray.jpg"))
   (let ((width 32)
         (height 32))
-    (let ((buf (make-array (* width height) :initial-element 42)))
+    (let ((buf (make-array (* width height) :initial-element 42 :element-type 'jpeg::uint8)))
       (time (jpeg:encode-image output buf 1 width height
                           :q-tabs *gray-q-tabs*)))))
