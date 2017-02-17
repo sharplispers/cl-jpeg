618398dda7852bb0496cd8e0c88e8640dafd64d1
Eugene Zaikonnikov <eugene@funcall.org>
2017-02-17 13:08:07 +0100

    more tests

--- test.lisp
+++ test.lisp
@@ -1,12 +1,20 @@
 (cl:in-package :jpeg)
 
-(defun test-reencode (&optional (pathname #P"/home/eugene/Pictures/nyc.jpg"))
+(defun test-reencode (&key (pathname #P"/home/eugene/Pictures/nyc.jpg") cached)
   (time (multiple-value-bind (buf h w nc)
-	    (jpeg:decode-image pathname)
+	    (jpeg:decode-image pathname :cached-source-p cached)
 	  (jpeg:encode-image #P"/tmp/test.jpg" buf nc h w))))
 
-(defun test-decode (&optional (pathname #P"/home/eugene/Pictures/nyc.jpg"))
-  (time (jpeg:decode-image pathname))
+(defun test-decode (&key (pathname #P"/home/eugene/Pictures/nyc.jpg") cached)
+  (time (jpeg:decode-image pathname :cached-source-p cached))
+  nil)
+
+(defun test-decode-reuse (&key (pathname #P"/home/eugene/Pictures/nyc.jpg"))
+  (let ((d (make-descriptor)))
+    (with-open-file (stream pathname :direction :input :element-type 'uint8)
+      (time (jpeg:decode-stream stream :cached-source-p t :descriptor d)))
+    (with-open-file (stream pathname :direction :input :element-type 'uint8)
+      (time (jpeg:decode-stream stream :cached-source-p t :descriptor d))))
   nil)
 
 (defun test-reencode-prealloc (&optional (pathname #P"/home/eugene/Pictures/nyc.jpg"))
@@ -24,3 +32,10 @@
     (let ((buf (make-array (* width height) :initial-element 42)))
       (time (jpeg:encode-image output buf 1 width height
                           :q-tabs *gray-q-tabs*)))))
+
+(defun test-flexi-decode (&optional (pathname #P"/home/eugene/Pictures/nyc.jpg"))
+  (time (with-open-file (stream pathname :direction :input :element-type 'uint8)
+    (let ((seq (make-array (file-length stream))))
+    (read-sequence seq stream)
+    (flexi-streams:with-input-from-sequence (is seq)  (jpeg:decode-stream is)))))
+  nil)
